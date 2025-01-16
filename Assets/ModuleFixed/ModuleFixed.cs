9cbea6702fa624c02f49960661234ea81a7ab765
MuHua-123 <136542559+MuHua-123@users.noreply.github.com>
2025-01-16 09:24:45 +0800

    Update ModuleFixed.cs

--- Assets/ModuleFixed/ModuleFixed.cs
+++ Assets/ModuleFixed/ModuleFixed.cs
@@ -6,5 +6,6 @@ using UnityEngine;
 /// 固定数据物体模块
 /// </summary>
 public abstract class ModuleFixed : MonoBehaviour {
-    
+    /// <summary> 核心模块 </summary>
+    protected virtual ModuleCore ModuleCore => ModuleCore.I;
 }
