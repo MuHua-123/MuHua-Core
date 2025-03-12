b7f6afc7d608fd7d7b89438c6db6ab4c3c466498
MuHua-123 <2960208585@qq.com>
2025-03-12 22:25:01 +0800

    Update ModuleUIPage.cs

--- Packages/UITool/Runtime/ModuleCoreUI/ModuleUIPage.cs
+++ Packages/UITool/Runtime/ModuleCoreUI/ModuleUIPage.cs
@@ -7,14 +7,16 @@ namespace MuHua {
 	/// <summary>
 	/// 页面模块
 	/// </summary>
-	public class ModuleUIPage : MonoBehaviour {
+	public abstract class ModuleUIPage : MonoBehaviour {
 		/// <summary> 绑定文档 </summary>
 		public UIDocument document;
 		/// <summary> 根目录文档 </summary>
 		public VisualElement root => document.rootVisualElement;
+		/// <summary> 绑定文档 </summary>
+		public abstract VisualElement Element { get; }
 		/// <summary> 添加UI元素 </summary>
-		public void Add(VisualElement child) => root.Add(child);
+		public void Add(VisualElement child) => Element.Add(child);
 		/// <summary> 查询UI元素 </summary>
-		public T Q<T>(string name = null, string className = null) where T : VisualElement => root.Q<T>(name, className);
+		public T Q<T>(string name = null, string className = null) where T : VisualElement => Element.Q<T>(name, className);
 	}
 }
