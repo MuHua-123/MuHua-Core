e4d10c15c656e5c02b1f24ddc34551aba4221691
MuHua-123 <136542559+MuHua-123@users.noreply.github.com>
2025-03-14 11:50:18 +0800

    修改样例包

---
+++ Assets/ModuleMuHua/LabelFollow/Script/StandardLabel.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MuHua;
+
+namespace MuHua.Sample
+{
+    public class StandardLabel : MonoBehaviour
+    {
+        public GameObject labelPrefab;
+        public Vector3 offset = new Vector3(0, 1, 0);
+        private GameObject labelObject;
+
+        void Start()
+        {
+            labelObject = LabelController.CreateLabel(transform, labelPrefab, offset);
+        }
+        void OnDestroy()
+        {
+            if (labelObject != null) { Destroy(labelObject); }
+        }
+    }
+}
+
