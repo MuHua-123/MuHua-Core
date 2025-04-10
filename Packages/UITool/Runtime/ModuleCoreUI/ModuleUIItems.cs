d6d945c2ce687d1c4245c853ed77f82d2524d3a5
MuHua-123 <136542559+MuHua-123@users.noreply.github.com>
2025-04-10 14:46:22 +0800

    1

--- Packages/UITool/Runtime/ModuleCoreUI/ModuleUIItems.cs
+++ Packages/UITool/Runtime/ModuleCoreUI/ModuleUIItems.cs
@@ -8,12 +8,17 @@ namespace MuHua {
 	/// <summary>
 	/// UI项容器
 	/// </summary>
-	public class ModuleUIItems<Item, Data> : ModuleUIPanel where Item : ModuleUIItem<Data> {
+	public class ModuleUIItems<T, Data> : ModuleUIPanel where T : ModuleUIItem<Data> {
 		public readonly VisualTreeAsset templateAsset;
-		public readonly Func<Data, VisualElement, Item> generate;
-		public List<Item> uiItems = new List<Item>();
+		public readonly Func<Data, VisualElement, T> generate;
+
+		public List<T> uiItems = new List<T>();
+
+		/// <summary> 数据操作 </summary
+		public virtual T this[int index] => uiItems[index];
+
 		/// <summary>  UI容器  </summary>
-		public ModuleUIItems(VisualElement element, VisualTreeAsset templateAsset, Func<Data, VisualElement, Item> generate) : base(element) {
+		public ModuleUIItems(VisualElement element, VisualTreeAsset templateAsset, Func<Data, VisualElement, T> generate) : base(element) {
 			this.templateAsset = templateAsset;
 			this.generate = generate;
 		}
@@ -21,7 +26,7 @@ namespace MuHua {
 		public void Release() {
 			element.Clear();
 			uiItems.ForEach(obj => obj.Release());
-			uiItems = new List<Item>();
+			uiItems = new List<T>();
 		}
 		/// <summary> 创建UI项 </summary>
 		public void Create(List<Data> datas) {
@@ -31,7 +36,7 @@ namespace MuHua {
 		/// <summary> 创建UI项 </summary>
 		public void Create(Data data) {
 			VisualElement element = templateAsset.Instantiate();
-			Item item = generate(data, element);
+			T item = generate(data, element);
 			this.element.Add(item.element);
 			uiItems.Add(item);
 		}
--- Packages/UITool/Runtime/ModuleCoreUI/ModuleUIPanel.cs
+++ Packages/UITool/Runtime/ModuleCoreUI/ModuleUIPanel.cs
@@ -7,10 +7,12 @@ namespace MuHua {
 	/// <summary>
 	/// UI控件
 	/// </summary>
-	public class ModuleUIPanel  {
+	public class ModuleUIPanel {
 		/// <summary> 绑定的元素 </summary>
 		public readonly VisualElement element;
 		/// <summary> UI控件 </summary>
 		public ModuleUIPanel(VisualElement element) => this.element = element;
+		/// <summary> 查询UI元素 </summary>
+		public T Q<T>(string name = null, string className = null) where T : VisualElement => element.Q<T>(name, className);
 	}
 }
